b0fadf0473aaaddfc113b6c2efe0667f9871ed04
Sofia <ssanchez@grupovestel.com>
2024-03-26 08:27:28 +0100

    Fin sección 10

---
+++ src/app/shared/components/loadingSpinner/loadingSpinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-loading-spinner',
+  templateUrl: './loadingSpinner.component.html',
+  styleUrl: './loadingSpinner.component.css'
+})
+
+export class LoadingSpinnerComponent { }
